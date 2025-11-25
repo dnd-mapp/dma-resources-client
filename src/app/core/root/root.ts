e7db7e48491055bd8db591c302471cffe6eb220d
NoNamer777 <oscar.wellner@gmail.com>
2025-11-25 17:15:23 +0100

    Add ChangeDetectionStrategy

--- src/app/core/root/root.ts
+++ src/app/core/root/root.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'dma-root',
     templateUrl: './root.html',
     styleUrl: './root.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [RouterOutlet],
 })
 export class Root {}

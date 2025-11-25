8407a988e1c436bdb2d32ebf11736a7b2729c8c2
NoNamer777 <oscar.wellner@gmail.com>
2025-11-25 17:23:46 +0100

    Add linting

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintPrettierConfig = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+    {
+        files: ['**/*.ts'],
+        extends: [
+            eslint.configs.recommended,
+            tseslint.configs.recommended,
+            tseslint.configs.stylistic,
+            angular.configs.tsRecommended,
+        ],
+        processor: angular.processInlineTemplates,
+        rules: {
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    type: 'attribute',
+                    prefix: 'dma',
+                    style: 'camelCase',
+                },
+            ],
+            '@angular-eslint/component-selector': [
+                'error',
+                {
+                    type: 'element',
+                    prefix: 'dma',
+                    style: 'kebab-case',
+                },
+            ],
+        },
+    },
+    {
+        files: ['**/*.html'],
+        extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+        rules: {},
+    },
+    eslintPrettierConfig,
+]);

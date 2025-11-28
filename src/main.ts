54922e76878777717f9959eb7ef58b1be4734fca
NoNamer777 <oscar.wellner@gmail.com>
2025-11-28 14:46:39 +0100

    Prettier

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig, Root } from './app';
 
-bootstrapApplication(Root, appConfig)
-  .catch((error) => console.error(error));
+bootstrapApplication(Root, appConfig).catch((error) => console.error(error));
